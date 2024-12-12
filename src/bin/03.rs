61d20a6bf5e76ae83ca0112ffc9a69fa5ac2bc79
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2024-12-12 23:43:11 +0000

    feat: solve day 3

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,3 +1,5 @@
+use regex::Regex;
+
 advent_of_code::solution!(3);
 
 pub fn part_one(input: &str) -> Option<u32> {
@@ -5,7 +7,35 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    Some(parse_input(input))
+}
+
+fn parse_input(input: &str) -> u32 {
+    let mut instr_en = true;
+    let mut sum = 0;
+    // One named capture group per instruction
+    let instr_patterns = [
+        r"(?<mul>mul\((?<num1>\d{1,3}),(?<num2>\d{1,3})\))",
+        r"(?<do>do\(\))",
+        r"(?<dont>don't\(\))",
+    ];
+    let re_pattern = format!(r"({})", instr_patterns.join("|"));
+    let re = Regex::new(&re_pattern);
+    match re {
+        Err(e) => eprintln!("regex pattern error: {}", e),
+        Ok(re) => {
+            for m in re.captures_iter(input) {
+                if m.name("do") != None {
+                    instr_en = true;
+                } else if m.name("dont") != None {
+                    instr_en = false;
+                } else if instr_en && m.name("mul") != None {
+                    sum += &m["num1"].parse::<u32>().unwrap() * &m["num2"].parse::<u32>().unwrap();
+                }
+            }
+        }
+    }
+    sum
 }
 
 fn parse_muls(input: &str) -> u32 {
@@ -100,13 +130,17 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        let result = part_one(&advent_of_code::template::read_file_part(
+            "examples", DAY, 1,
+        ));
         assert_eq!(result, Some(161));
     }
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
         assert_eq!(result, Some(48));
     }
 }
