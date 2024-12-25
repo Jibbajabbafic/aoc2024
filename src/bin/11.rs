a8db01f6eecb75ac7c9e6764d5f23f95757745b2
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2024-12-25 16:22:13 +0000

    feat: solve day 11 with dfs and memoization

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -1,10 +1,10 @@
-use std::collections::{HashMap, VecDeque};
+use std::collections::HashMap;
 
 advent_of_code::solution!(11);
 
 struct FunStones {
     current: Vec<u64>,
-    solved: Vec<u64>,
+    solved_stones_cache: HashMap<u32, HashMap<u64, u64>>,
 }
 
 impl FunStones {
@@ -14,7 +14,7 @@ impl FunStones {
                 .split_ascii_whitespace()
                 .map(|c| c.parse().unwrap())
                 .collect(),
-            solved: Vec::new(),
+            solved_stones_cache: HashMap::new(),
         }
     }
 
@@ -38,43 +38,33 @@ impl FunStones {
     }
 
     // depth first search to get the totals instead of manually doing each iteration
-    fn find_total_dfs(&self, max_depth: u32) -> u64 {
-        // let mut search_queue = VecDeque::<u64>::from(self.current.clone());
+    fn find_total_dfs(&mut self, max_depth: u32) -> u64 {
         let mut total_stones = 0;
-        for &stone in &self.current {
+        for stone in self.current.clone() {
             total_stones += self.recursive_solve_stone(stone, max_depth, 1);
         }
         return total_stones;
-        // let mut solved_cache = HashMap::<u32, HashMap<u64, u64>>::new();
-        // let mut visited_depth = 0;
-        // do something for dfs and memoisation
-        // traverse stones, track each one in a stack, store result as we leave the stack again?
-        // while let Some(stone) = search_queue.pop_front() {
-        //     // if let Some(num_map) = solved_cache.get(depth) {
-        //     //     if let Some(solution) = num_map.get(&stone) {}
-        //     // }
-        //     // track which ones already visited
-        //     // visited_stack.push(stone);
-        //     if stone == 0 {
-        //         search_queue.push_front(1);
-        //     } else if stone.to_string().chars().count() % 2 == 0 {
-        //         // do the split
-        //         let digits = stone.to_string();
-        //         let mid = digits.len() / 2;
-        //         let (d1, d2) = digits.split_at(mid);
-        //         search_queue.push_front(d1.parse().unwrap());
-        //         search_queue.push_front(d2.parse().unwrap());
-        //     } else {
-        //         search_queue.push_front(stone * 2024);
-        //     }
-        // }
     }
 
-    fn recursive_solve_stone(&self, stone: u64, depth_limit: u32, cur_depth: u32) -> u64 {
+    fn recursive_solve_stone(&mut self, stone: u64, depth_limit: u32, cur_depth: u32) -> u64 {
+        if let Some(depth_cache) = self.solved_stones_cache.get(&cur_depth) {
+            if let Some(solution) = depth_cache.get(&stone) {
+                // this stone has already been solved for this level of depth, so stop calculating
+                return *solution;
+            }
+        }
         let next_stones = FunStones::apply_rules(stone);
         // reached limit so stop and return current answer, this will be summed as we go back up the stack
         if cur_depth == depth_limit {
-            return next_stones.len() as u64;
+            let total_stones = next_stones.len() as u64;
+            // cache this result for future searches
+            if let Some(depth_cache) = self.solved_stones_cache.get_mut(&cur_depth) {
+                depth_cache.insert(stone, total_stones);
+            } else {
+                self.solved_stones_cache
+                    .insert(cur_depth, HashMap::from([(stone, total_stones)]));
+            }
+            return total_stones;
         }
         // search one stone first to max depth, add up totals from both searches
         let mut total_stones = 0;
@@ -82,6 +72,13 @@ impl FunStones {
         // then search second one if exists
         if next_stones.len() > 1 {
             total_stones += self.recursive_solve_stone(next_stones[1], depth_limit, cur_depth + 1);
+        }
+        // cache this result for future searches
+        if let Some(depth_cache) = self.solved_stones_cache.get_mut(&cur_depth) {
+            depth_cache.insert(stone, total_stones);
+        } else {
+            self.solved_stones_cache
+                .insert(cur_depth, HashMap::from([(stone, total_stones)]));
         }
         return total_stones;
     }
@@ -102,13 +99,13 @@ impl FunStones {
 }
 
 pub fn part_one(input: &str) -> Option<u64> {
-    let stones = FunStones::from_input(input);
+    let mut stones = FunStones::from_input(input);
     let dfs_answer = stones.find_total_dfs(25);
     Some(dfs_answer)
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    let stones = FunStones::from_input(input);
+    let mut stones = FunStones::from_input(input);
     let dfs_answer = stones.find_total_dfs(75);
     Some(dfs_answer)
 }
@@ -123,9 +120,9 @@ mod tests {
         assert_eq!(result, Some(55312));
     }
 
-    #[test]
-    fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
-    }
+    // #[test]
+    // fn test_part_two() {
+    //     let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+    //     assert_eq!(result, None);
+    // }
 }
